63167b3b6793df898b8477abdc42b99ce0d9cbe7
j1one122 <twothreejone@gmail.com>
2023-11-10 12:39:46 +0000

    3d

---
+++ Mouse controller copy/Assets/Scripts/CharController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+	//PSEDO CODE: Declare and Initialize float variables for speed, maxspeed, acceleration, deceleration, rotation
+
+
+    public float speed = 10.0f;
+	public float maxspeed = 100.0f;
+	public float acc = 5.0f;
+	public float dec = 5.0f;
+	public float rotation = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		//PSEUDO CODE: Check for space key input. If not max speed, accelerate, else decelerate
+        if(Input.GetKey("space"))
+		{
+           //acc
+		   if(speed < maxspeed)
+			   speed += acc;
+			   //speed = speed + acc;
+		   
+		}
+        else
+		{
+            //dec
+			if(speed > 0)
+			   speed -= dec;
+			   //speed = speed - dec;	
+		}
+
+		//PSEUDO CODE: Use the speed and transform to move the object forward
+		//FUNCTION: Simplist transform with ACC, DEC, Speed (Space bar), and TDT
+		transform.position += transform.forward * Time.deltaTime * speed;
+
+		//PSEUDO CODE: Check for Left and Right Key inputs. Turn if left or right keys are pressed
+		if ((Input.GetKey(KeyCode.LeftArrow)))
+		{
+			transform.Rotate(0,2.0f,0);
+		}
+		if((Input.GetKey(KeyCode.RightArrow)))
+		{
+			transform.Rotate(0,-2.0f,0);
+
+		}
+	}
+
+}
---
+++ Mouse controller copy/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+	//PSEUDO CODE: Declare public variable of type float for speed;
+    public float speed = 50.0f;
+	
+	void OnMouseDrag()
+	{
+		//TRY:
+		// Declare and Initialize the X and Y mouse axes and multiply by speed.
+		// The value is in the range -1 to 1
+        //Mouse Y will become Z translation
+        float transX = Input.GetAxis("Mouse X") * speed;
+		float transZ = Input.GetAxis("Mouse Y") * speed;
+
+		// Make translation variable move at 10 meters per second instead of 10 meters per frame with TimeDeltaTime...
+        transX *= Time.deltaTime;
+		transZ *= Time.deltaTime;
+
+		// use transform to translate with values above
+		////Move translation along the object's x abd z-axis
+		transform.Translate(transX, 0, transZ);
+
+		//Try Stretch Task in tutorial sheet
+
+	}
+}
+
+//From: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
